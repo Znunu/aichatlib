480694e9d157bebdfc400dbeddaf8c0384c62aa1
Znunu <61394004+Znunu@users.noreply.github.com>
2024-05-16 19:16:46 +0200

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,8 +1,23 @@
+import asyncio
 import json
+from datetime import datetime
+import httpx
+import util
 from docstring_parser import parse as parse_doc
+from litellm import acompletion
+import beta.message as message_types
+
+
+client = httpx.AsyncClient()
+
+
+class Forget(Exception):
+    pass
+
 
 class BaseTools:
     tool_functions: []
+    usage: {}  # = {util.Models.GPT: 300, util.Models.CLAUDE: 850}
 
     def __init__(self, tool_functions):
         self.tool_functions = tool_functions
@@ -10,14 +25,14 @@ class BaseTools:
     def get_all_json_docs(self):
         return [self.create_json_docstring(fun) for fun in self.tool_functions]
 
-    async def execute_tool_call(self, ctx, tool_call):
-        for tool_function in self.tool_functions:
-            if tool_call.function.name == tool_function.__name__:
-                params = json.loads(tool_call.function.arguments)
-                print(f"Function {tool_call.function.name} called with {params}")
-                params["ctx"] = ctx
-                response = await tool_function(**params)
-                return {"role": "tool", "tool_call_id": tool_call.id, "content": response, "name": tool_call.function.name}
+    def new_tool_call_msg(self, msg, extra_args=None):
+        calls = []
+        for call in msg.tool_calls:
+            fun = next(f for f in self.tool_functions if f.__name__ == call.function.name)
+            args = json.loads(call.function.arguments)
+            if extra_args: args.extend(extra_args)
+            calls.append(message_types.ToolCallMessage.Call(fun, call.id, args))
+        return message_types.ToolCallMessage(calls)
 
     @staticmethod
     def create_json_docstring(func):
@@ -33,4 +48,27 @@ class BaseTools:
             json_docs["function"]["parameters"]["properties"][param.arg_name] = param_docs
         json_docs["function"]["parameters"]["required"] = required
         return json_docs
+
+    async def token_usage(self, model: util.Model_Class):
+        await self.tokens_for_tools()
+        return self.usage[model]
+
+    async def tokens_for_tools(self):
+        async def run(model, specs):
+            response = await asyncio.wait_for(acompletion(
+                model=model,
+                messages=[{"role": "user", "content": "."}],
+                temperature=1,
+                max_tokens=200,
+                tools=specs
+            ), timeout=30)
+            return response["usage"]["prompt_tokens"]
+
+        self.usage = {}
+        for model in (util.strong_gpt_version, util.strong_claude_version):
+            res_without_tools = await run(model, None)
+            res_with_tools = await run(model, self.get_all_json_docs())
+            delta = res_with_tools - res_without_tools
+            print(f"{model}: {delta}")
+            self.usage[model] = delta
 
